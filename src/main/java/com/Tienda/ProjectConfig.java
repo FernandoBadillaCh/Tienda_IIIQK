123ed15e7d36ea8a7c9507d2899ad3b0465e2e08
Fernando Badilla Chinchilla <133604779+FernandoBadillaCh@users.noreply.github.com>
2023-10-03 21:12:20 -0600

    Lec 04

--- src/main/java/com/Tienda/ProjectConfig.java
+++ src/main/java/com/Tienda/ProjectConfig.java
@@ -11,8 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-
-
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
 
